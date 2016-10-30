c579e1a6cca3fa6fc94d6b161f6dbcf3dfa307a0
Pedro Camacho <pedrojesus.camacho@universidadeuropea.es>
2016-10-30 12:17:57 +0100

    Init Commit

---
+++ UD2-Tarea2-Prioridades/src/Avisador/Correo.java
@@ -0,0 +1,20 @@
+package Avisador;
+
+public class Correo {
+	private boolean correoNuevo;
+	
+	public Correo (){
+		this.correoNuevo=false;
+	}
+	
+	public boolean hayCorreo() {		
+		return correoNuevo;
+	}
+	public void leer(){
+		this.correoNuevo=false;
+	}
+	public void nuevo (){
+		this.correoNuevo=true;
+	}
+
+}
